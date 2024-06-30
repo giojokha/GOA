b25e2620541bf955f5e03104b96cc16e7873e3de
giojokha <128926412+giojokha@users.noreply.github.com>
2024-06-30 15:01:41 +0400

    Add files via upload

---
+++ Day 48/homework/index4.js
@@ -0,0 +1,6 @@
+let firstName = "Giorgi";
+let lastName = "Jokhadze";
+let age = 16;
+let location = "Georgia_Tbilisi";
+
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old and I live in ${location}.`);
