69102fd147d2a401306eaba253eefea0588df3cb
giojokha <128926412+giojokha@users.noreply.github.com>
2024-11-11 21:22:16 +0400

    Add files via upload

---
+++ lvl110/dav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function dav(props) {
+  return (
+    <div>
+      <h2>{props.value}</h2>
+      <h3>{props.Dollar}</h3>
+    </div>
+  )
+}
---
+++ lvl110addons/app2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../lvl110/dav'
+
+export default function app2() {
+  const myInfo = "MiniWebSite"
+  return (
+    <div>
+      <button value={Number_One_Davaleba} Dollar={BenjaminFranklin} />
+    </div>
+  )
+}
